b1311f24d8bd04d01eeb63e4e7db4610623b48b0
Caleb <psychotix@hotmail.com>
2019-12-03 21:28:33 -0600

    merge with master

---
+++ gradientdescent.py
@@ -0,0 +1,39 @@
+import numpy as np
+from g import g
+
+
+def gradient_descent(X, Y, alpha):
+  print("running gradient descent")
+  X = np.array(X)
+  Y = np.array(Y)
+  
+  samples, features = X.shape
+  X = np.hstack((np.ones((samples, 1)), X))
+
+  samples, features = X.shape
+
+  theta_matrix = np.random.rand(features, 1)
+  derivative_matrix = np.ones((features, 1))
+
+  
+  iteration = 1
+  converged = False
+  while not converged:
+    for x in derivative_matrix:
+      # If the derivative has converged
+      if abs(x) <= 0.001:
+        converged = True
+
+    h_theta = g(np.matmul(X, theta_matrix))
+
+    for i in range(0, features):
+      derivative_matrix[i] = [1 / float(samples)] * np.matmul(np.transpose(np.subtract(h_theta, Y)), X[:,i])
+
+    theta_matrix = theta_matrix - (alpha * derivative_matrix)
+    
+    iteration += 1
+
+    if converged:
+      break
+  
+  return theta_matrix
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import createdata
+import gradientdescent as gd
 
 train_data = "music/train"
 test_data = "music/test"
 
 createdata.convert_to_features(train_data)
+
+X_and_y_matrix = createdata.read_feature_data(train_data)
+
+print(gd.gradient_descent(X_and_y_matrix[:, 1:], X_and_y_matrix[:, 0], 0.7))
