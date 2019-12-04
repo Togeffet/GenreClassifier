5d00198e852ff3f6ffa7a55ef7ff62990340a912
Franklin Fanelli <togeffet@gmail.com>
2019-12-04 00:23:13 -0600

    Fix gradient descent algorithm

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import os
 train_data = "music/train"
 test_data = "music/test"
 
-if not os.path.exists("music/train/features.npy"):
-  createdata.convert_to_features(train_data)
+if not os.path.exists("music/train/train_features.npy"):
+  createdata.convert_to_features(train_data, "train_features.py")
 
-X_and_y_matrix = createdata.read_feature_data(train_data)
+X_and_y_matrix = createdata.read_feature_data(train_data + "/train_features.npy")
 
 theta_matrix = gd.gradient_descent(X_and_y_matrix[:, 1:], X_and_y_matrix[:, 0], 0.07)
 print(theta_matrix)
