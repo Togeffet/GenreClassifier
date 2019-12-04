7908337333c772a0f8261c2c058745d34af9f87a
Caleb <psychotix@hotmail.com>
2019-12-03 18:16:09 -0600

    changed songread.py to createdata.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import songread
 
-audio_path = 'music/touch_the_sky.mp3'
-zc, sc, sr, mf, chroma = songread.read_song_file(audio_path)
+train_data = "music/train"
+test_data = "music/test"
 
-print("""Zero Crossings: {}\n
-         Specral Centroids: {}\n\
-         Spectral Rolloff:{}\n\
-         MFCCS: {}\n\
-         Chroma Freqs: {}\n""".format(zc, sc, sr, mf, chroma))
