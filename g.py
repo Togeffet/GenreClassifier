f84ef113b32a9e2027f9875799a86be359d41d46
Franklin Fanelli <togeffet@gmail.com>
2019-12-03 21:11:50 -0600

    Create gradient descent function

---
+++ g.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def g(z):
+  return float(1) / (1 + np.exp(-z))
--- main.py
+++ main.py
@@ -1,10 +1,29 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
 import songread
+from gradientdescent import gradient_descent
 
-audio_path = 'music/touch_the_sky.mp3'
-zc, sc, sr, mf, chroma = songread.read_song_file(audio_path)
+#audio_path = 'music/touch_the_sky.mp3'
+#zc, sc, sr, mf, chroma = songread.read_song_file(audio_path)
 
-print("""Zero Crossings: {}\n
-         Specral Centroids: {}\n\
-         Spectral Rolloff:{}\n\
-         MFCCS: {}\n\
-         Chroma Freqs: {}\n""".format(zc, sc, sr, mf, chroma))
+#print("""Zero Crossings: {}\n
+#         Specral Centroids: {}\n\
+#         Spectral Rolloff:{}\n\
+#         MFCCS: {}\n\
+#         Chroma Freqs: {}\n""".format(zc, sc, sr, mf, chroma))
+
+X = [
+  [0.829612087, 0.829612087],
+  [0.829612087, 0.829612087],
+  [0.829612087, 0.829612087],
+  [0.829612087, 0.829612087]
+]
+Y = [
+  [1],
+  [1],
+  [0],
+  [0]
+]
+alpha = .001
+print(gradient_descent(X, Y, alpha))
