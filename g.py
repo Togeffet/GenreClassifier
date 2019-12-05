Fix overflow error in exponent function in sigmoid function

g.py
@@ -1,4 +1,4 @@
 import numpy as np
 
 def g(z):
-  return 1.0 / (1.0 + np.exp(-z))
+  return 1.0 / (1.0 + np.exp(np.array(-z, dtype=np.float128)))